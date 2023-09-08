aa9012dd2e7f340bd9557ad67eccc30d08cb94e8
rizky.pratama <rizky.pratama@sicepat.com>
2023-09-09 00:37:00 +0700

    refactor(mathBasic): refactor validator & testing position

--- src/MathBasic.js
+++ src/MathBasic.js
@@ -1,49 +1,23 @@
+const Validator = require('./validator');
+
 const MathBasic = {
     add: (...args) => {
-        if (args.length !== 2) {
-            throw new Error('fungsi add hanya menerima dua parameter');
-        }
-
-        const [a, b] = args;
-
-        if (typeof a !== 'number' || typeof b !== 'number') {
-            throw new Error('fungsi hanya menerima parameter number')
-        }
+        const [a, b] = Validator.args(args, 2);
 
         return a + b;
     },
     subtract: (...args) => {
-        if (args.length !== 2) {
-            throw new Error('fungsi substract hanya menerima dua parameter');
-        }
+        const [a, b] = Validator.args(args, 2);
 
-        const [a, b] = args;
-
-        if (typeof a !== 'number' || typeof b !== 'number') {
-            throw new Error('fungsi hanya menerima parameter number')
-        }
-
-        return a-b;
+        return a - b;
     },
     multiply: (...args) => {
-        if (args.length !== 2) {
-            throw new Error('fungsi multiply hanya menerima dua parameter');
-        }
+        const [a, b] = Validator.args(args, 2);
 
-        const [a, b] = args;
-
-        if (typeof a !== 'number' || typeof b !== 'number') {
-            throw new Error('fungsi hanya menerima parameter number')
-        }
-
-        return a*b;
+        return a * b;
     },
     divide: (...args) => {
-        if (args.length !== 2) throw new Error('Fungsi hanya menerima dua parameter');
-
-        const [a, b] = args;
-
-        if (typeof a !== 'number' || typeof b !== 'number') throw new Error('fungsi hanya menerima parameter number');
+        const [a, b] = Validator.args(args, 2);
 
         return a / b;
     },
